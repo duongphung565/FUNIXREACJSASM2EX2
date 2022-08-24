c4136270b82d164b0d37298cb27f8898dc687dff
duongphung565 <nguyenphungduong565@gmail.com>
2022-08-24 23:47:49 +0700

    commit 2 Department

--- src/components/DepartmentComponent.js
+++ src/components/DepartmentComponent.js
@@ -4,24 +4,7 @@ import {
     Card, CardText,
     CardTitle
 } from 'reactstrap';
-function RenderDepartment({ departments }) {
-    return (
-        <div className='row'>
-            {
-                departments.map((department) => {
-                    return (
-                        <div key={department.id} className='col-lg-4 col-md-6 col-12'>
-                            <Card className={'p-2 m-2'}>
-                                <CardTitle>{department.name}</CardTitle>
-                                <CardText>Số lượng nhân viên: {department.numberOfStaff}</CardText>
-                            </Card>
-                        </div>
-                    );
-                })
-            }
-        </div>
-    );
-}
+
 const Department = (props) => {
     if (props.departments != null) {
         return (
